ff55abd869b1030f61bf276e8289a4b9e8ac46cc
jferdizzle <julianferdman@gmail.com>
2022-03-03 08:59:21 -0500

    remove collapse on nav for now, clean up files

--- src/redux/blockchain/blockchainActions.js
+++ src/redux/blockchain/blockchainActions.js
@@ -1,7 +1,7 @@
 // constants
 import Web3EthContract from "web3-eth-contract";
 import Web3 from "web3";
-import SmartContract from "../../contracts/TBCNFTABI.json";
+import SmartContract from "../../contracts/mutantABI.json";
 // log
 import { fetchData } from "../data/dataActions";
 
