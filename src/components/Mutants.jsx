0fa334f1e72f331bd878b3b36b87d2262f03de8c
jferdizzle <julianferdman@gmail.com>
2022-06-17 17:36:56 -0400

    copy changes

--- src/components/Mutants.jsx
+++ src/components/Mutants.jsx
@@ -52,7 +52,7 @@ function Mutants() {
                   <div className="mb-5  row">
                     <div className="mb-5 mb-lg-0 col-lg-7 col-12">
                       <h1 className="common-title mb-3">THE MACC</h1>
-                      <p className="common-p  text-justify">The MUTANT GRANDPA APE COUNTRY CLUB is a collection of up to 10,000 Mutant Apes that can only be created by exposing an existing Grandpa Ape to a vial of MUTANT SERUM or by minting a Mutant Ape in the public sale.</p>
+                      <p className="common-p  text-justify">The MUTANT APE COUNTRY CLUB is a collection of up to 10,000 Mutant Apes that can only be created by exposing an existing Grandpa Ape to a vial of MUTANT SERUM or by minting a Mutant Ape in the public sale.</p>
                       <p className="common-p text-justify">The MACC is a way to reward our ape holders with an entirely new NFT—a “mutant” version of their ape—while also allowing newcomers into the GACC ecosystem at a lower tier of membership. Mutants represent the final tier of membership; everything going forward occurs with the intention of accruing utility and member’s-only benefits to Grandpa Apes foremost, but also Mutants, and to a lesser extent, Grandpa Apes with GAKC companions.</p>
                     </div>
                     <div className="my-lg-auto col-lg-4 col-12 offset-lg-1">
@@ -129,7 +129,7 @@ function Mutants() {
                     <div className="mb-5 mb-lg-0 col-lg-7 col-12 order-lg-last">
                       <h1 className="common-title mb-3">DISTRIBUTION &amp; PRICING</h1>
                       <p className="common-p text-justify">A total of 5,000 Mutant Serums have been airdropped to all GACC token holders.</p>
-                      <p className="common-p text-justify">2,500 Mutant Apes will be reserved for Whitelist Wallets to be minted during the Whitelist Minting period, and it will cost “X” ETH to mint a Mutant. After the Whitelist Mint period closes, 2,500 more Mutant Apes will be available to mint in a public Dutch Auction. The price to mint a Mutant will start at “X” ETH, and gradually reduce to .01 ETH over the course of 3 hours. This is to allow the community to set a fair starting price and, hopefully, avoid any gas war.</p>
+                      <p className="common-p text-justify">2,500 Mutant Apes will be reserved for Whitelist Wallets to be minted during the Whitelist Minting period, and it will cost 0.15 ETH to mint a Mutant. After the Whitelist Mint period closes, 2,500 more Mutant Apes will be available to mint in a public Dutch Auction. The price to mint a Mutant will start at 0.5 ETH, and gradually reduce to .01 ETH over the course of 6 hours. This is to allow the community to set a fair starting price and, hopefully, avoid any gas war.</p>
                       <p className="common-p text-justify">At the time of minting, Mutant Apes will be unrevealed. Once the public sale has concluded, all Mutant Apes will be revealed.</p>
                       <p className="common-p text-justify mb-0">After the mutants are revealed, GACC members will be able to begin mutating their apes with serums here on the site.</p>
                     </div>
