e3b3928e2dff37a96615aef351bce6a615b88250
jferdizzle <julianferdman@gmail.com>
2022-10-05 13:31:47 -0400

    add mega serums

---
+++ migrations/20221005172912-mega-serum-images.js
@@ -0,0 +1,306 @@
+"use strict";
+
+var metadata = [
+  {
+    token: "15000",
+    metadata: {
+      name: "Mutant Grandpa #15000",
+      attributes: [{ trait_type: "Legendary Grandpa", value: "Mega DMT" }],
+      image:
+        "https://ipfs.io/ipfs/QmSBVP9EyPaA2QxH8Q4du8CCB84acusjsPmMTQx5mkhYRT",
+      image_url:
+        "https://ipfs.io/ipfs/QmSBVP9EyPaA2QxH8Q4du8CCB84acusjsPmMTQx5mkhYRT",
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    token: "15001",
+    metadata: {
+      name: "Mutant Grandpa #15001",
+      attributes: [
+        { trait_type: "Legendary Grandpa", value: "Mega Golden Brown" },
+      ],
+      image:
+        "https://ipfs.io/ipfs/QmbuGhTwTKhAV442WWtvFQUo6bBh5DXRv3h2EFGVwMTUMR",
+      image_url:
+        "https://ipfs.io/ipfs/QmbuGhTwTKhAV442WWtvFQUo6bBh5DXRv3h2EFGVwMTUMR",
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    token: "15002",
+    metadata: {
+      name: "Mutant Grandpa #15002",
+      attributes: [
+        { trait_type: "Legendary Grandpa", value: "Mega Glitchy PC" },
+      ],
+      image:
+        "https://ipfs.io/ipfs/QmSadkB9tN8cUo4mi39hdrxYnkrTkXoozijTEfhNQzbsME",
+      image_url:
+        "https://ipfs.io/ipfs/QmSadkB9tN8cUo4mi39hdrxYnkrTkXoozijTEfhNQzbsME",
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    token: "15003",
+    metadata: {
+      name: "Mutant Grandpa #15003",
+      attributes: [{ trait_type: "Legendary Grandpa", value: "Mega Grey" }],
+      image:
+        "https://ipfs.io/ipfs/QmXk1sdC7TVw7GR6ioEiE6ggaikL4p3Sb8s4VhhVH1gE9E",
+      image_url:
+        "https://ipfs.io/ipfs/QmXk1sdC7TVw7GR6ioEiE6ggaikL4p3Sb8s4VhhVH1gE9E",
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    token: "15004",
+    metadata: {
+      name: "Mutant Grandpa #15004",
+      attributes: [
+        { trait_type: "Legendary Grandpa", value: "Mega Dark Brown" },
+      ],
+      image:
+        "https://ipfs.io/ipfs/QmeipPh5eJ3Yi2EwN2Lt5UB5xktGUTdoWnsPEZMEYfhgsn",
+      image_url:
+        "https://ipfs.io/ipfs/QmeipPh5eJ3Yi2EwN2Lt5UB5xktGUTdoWnsPEZMEYfhgsn",
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    token: "15005",
+    metadata: {
+      name: "Mutant Grandpa #15005",
+      attributes: [
+        { trait_type: "Legendary Grandpa", value: "Mega Death Bot" },
+      ],
+      image:
+        "https://ipfs.io/ipfs/QmSJFPw3PtNPHbiNf2AQQKr5mTEy66V5wVnYQBpXqznV6w",
+      image_url:
+        "https://ipfs.io/ipfs/QmSJFPw3PtNPHbiNf2AQQKr5mTEy66V5wVnYQBpXqznV6w",
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    token: "15006",
+    metadata: {
+      name: "Mutant Grandpa #15006",
+      attributes: [{ trait_type: "Legendary Grandpa", value: "Mega Red" }],
+      image:
+        "https://ipfs.io/ipfs/QmNY2ZMyLj6SpAkF3eztjEGQ1rk4ddgnNCddyuqokcpD9i",
+      image_url:
+        "https://ipfs.io/ipfs/QmNY2ZMyLj6SpAkF3eztjEGQ1rk4ddgnNCddyuqokcpD9i",
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    token: "15007",
+    metadata: {
+      name: "Mutant Grandpa #15007",
+      attributes: [{ trait_type: "Legendary Grandpa", value: "Mega Diamond" }],
+      image:
+        "https://ipfs.io/ipfs/QmWMmqXV1kDAHqaktZV39WKp6x3bdAdH2QRo5wDJ3A1xte",
+      image_url:
+        "https://ipfs.io/ipfs/QmWMmqXV1kDAHqaktZV39WKp6x3bdAdH2QRo5wDJ3A1xte",
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    token: "15008",
+    metadata: {
+      name: "Mutant Grandpa #15008",
+      attributes: [{ trait_type: "Legendary Grandpa", value: "Mega Cream" }],
+      image:
+        "https://ipfs.io/ipfs/Qmaqjfnbq7Si2DuKvQ57MxMHq8SdzRV6cvamKUxaMgio9i",
+      image_url:
+        "https://ipfs.io/ipfs/Qmaqjfnbq7Si2DuKvQ57MxMHq8SdzRV6cvamKUxaMgio9i",
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    token: "15009",
+    metadata: {
+      name: "Mutant Grandpa #15009",
+      attributes: [{ trait_type: "Legendary Grandpa", value: "Mega Cheetah" }],
+      image:
+        "https://ipfs.io/ipfs/QmXXyTB9iF7ytUSSNMD5a8NJYSrLhtQA6JxKn8vGiKFnir",
+      image_url:
+        "https://ipfs.io/ipfs/QmXXyTB9iF7ytUSSNMD5a8NJYSrLhtQA6JxKn8vGiKFnir",
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    token: "15010",
+    metadata: {
+      name: "Mutant Grandpa #15010",
+      attributes: [{ trait_type: "Legendary Grandpa", value: "Mega Black" }],
+      image:
+        "https://ipfs.io/ipfs/QmRsnZJ97GsdxrtSvoDZ9jGANbLMNm98rSwsm7hpezCdLC",
+      image_url:
+        "https://ipfs.io/ipfs/QmRsnZJ97GsdxrtSvoDZ9jGANbLMNm98rSwsm7hpezCdLC",
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    token: "15011",
+    metadata: {
+      name: "Mutant Grandpa #15011",
+      attributes: [{ trait_type: "Legendary Grandpa", value: "Mega White" }],
+      image:
+        "https://ipfs.io/ipfs/QmdFRuwzLzA1TDgWAfPDEGwXL5fqBdzXvm7tr9qkMP5XLU",
+      image_url:
+        "https://ipfs.io/ipfs/QmdFRuwzLzA1TDgWAfPDEGwXL5fqBdzXvm7tr9qkMP5XLU",
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    token: "15012",
+    metadata: {
+      name: "Mutant Grandpa #15012",
+      attributes: [{ trait_type: "Legendary Grandpa", value: "Mega Zombie" }],
+      image:
+        "https://ipfs.io/ipfs/QmbKc57FDv9i5vjdEjaF1qz3zo2Gzo96SjqiSH9E8XnWgj",
+      image_url:
+        "https://ipfs.io/ipfs/QmbKc57FDv9i5vjdEjaF1qz3zo2Gzo96SjqiSH9E8XnWgj",
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    token: "15013",
+    metadata: {
+      name: "Mutant Grandpa #15013",
+      attributes: [{ trait_type: "Legendary Grandpa", value: "Mega Gold" }],
+      image:
+        "https://ipfs.io/ipfs/QmXv3crUL7GTpkeMquxnaMo592TetLHuoHaRNorWPTd8nJ",
+      image_url:
+        "https://ipfs.io/ipfs/QmXv3crUL7GTpkeMquxnaMo592TetLHuoHaRNorWPTd8nJ",
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    token: "15014",
+    metadata: {
+      name: "Mutant Grandpa #15014",
+      attributes: [{ trait_type: "Legendary Grandpa", value: "Mega Pink" }],
+      image:
+        "https://ipfs.io/ipfs/QmbvA4vfyUY8hnWQiyLn96UHeqZ5idB2pQQZqmHycYzWWH",
+      image_url:
+        "https://ipfs.io/ipfs/QmbvA4vfyUY8hnWQiyLn96UHeqZ5idB2pQQZqmHycYzWWH",
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    token: "15015",
+    metadata: {
+      name: "Mutant Grandpa #15015",
+      attributes: [{ trait_type: "Legendary Grandpa", value: "Mega Tan" }],
+      image:
+        "https://ipfs.io/ipfs/QmcQ56ScGyFCS5UGx99Kpm8gGQwsvoY2Agnab6sPG2Qx1T",
+      image_url:
+        "https://ipfs.io/ipfs/QmcQ56ScGyFCS5UGx99Kpm8gGQwsvoY2Agnab6sPG2Qx1T",
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    token: "15016",
+    metadata: {
+      name: "Mutant Grandpa #15016",
+      attributes: [{ trait_type: "Legendary Grandpa", value: "Mega Trippy" }],
+      image:
+        "https://ipfs.io/ipfs/QmUmhw76gVAhK2THD6Dh97bXdfH1njrV7ioF5xMeY7M6NN",
+      image_url:
+        "https://ipfs.io/ipfs/QmUmhw76gVAhK2THD6Dh97bXdfH1njrV7ioF5xMeY7M6NN",
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    token: "15017",
+    metadata: {
+      name: "Mutant Grandpa #15017",
+      attributes: [{ trait_type: "Legendary Grandpa", value: "Mega Robot" }],
+      image:
+        "https://ipfs.io/ipfs/QmZmCSAzmKmhowwN5kAwb841krEPyRTbDrxrxdqxS4yGH9",
+      image_url:
+        "https://ipfs.io/ipfs/QmZmCSAzmKmhowwN5kAwb841krEPyRTbDrxrxdqxS4yGH9",
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    token: "15018",
+    metadata: {
+      name: "Mutant Grandpa #15018",
+      attributes: [{ trait_type: "Legendary Grandpa", value: "Mega Blue" }],
+      image:
+        "https://ipfs.io/ipfs/QmYkcXQvkjZV4c3HF6jsKfpcFsmfKvC8CUB4h3JyuMjBcr",
+      image_url:
+        "https://ipfs.io/ipfs/QmYkcXQvkjZV4c3HF6jsKfpcFsmfKvC8CUB4h3JyuMjBcr",
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    token: "15019",
+    metadata: {
+      name: "Mutant Grandpa #15019",
+      attributes: [{ trait_type: "Legendary Grandpa", value: "Mega Brown" }],
+      image:
+        "https://ipfs.io/ipfs/QmctowKVnhP7JCUM44LFfarKw7Wx2NZj4oQeDLxYtos3JB",
+      image_url:
+        "https://ipfs.io/ipfs/QmctowKVnhP7JCUM44LFfarKw7Wx2NZj4oQeDLxYtos3JB",
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    token: "15020",
+    metadata: {
+      name: "Mutant Grandpa #15020",
+      attributes: [{ trait_type: "Legendary Grandpa", value: "Mega Noise" }],
+      image:
+        "https://ipfs.io/ipfs/QmdSzSe4eFhSprYiW87nphwSFUp7fUDQeYMzNhVb9Xpbre",
+      image_url:
+        "https://ipfs.io/ipfs/QmdSzSe4eFhSprYiW87nphwSFUp7fUDQeYMzNhVb9Xpbre",
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
+
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert(
+      "RevealedMetadata",
+      metadata,
+      {},
+      { metadata: { type: new Sequelize.JSON() } }
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
