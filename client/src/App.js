f0f923f4967119df1e78d026e8e7acb84a8781d6
cartoonitunes <181663414+cartoonitunes@users.noreply.github.com>
2023-02-23 20:29:02 -0500

    add iframe

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
  
 import Mutants from './components/Mutants';
 import Home from './components/Home';
+import Marketplace from './components/Marketplace';
 import Teaser from './components/Teaser';
 import Navigation from './components/Navigation';
  
@@ -15,6 +16,11 @@ class App extends Component {
             <Switch>
              <Route path="/" component={Home} exact/>
              <Route path="/home" component={Home}/>
+             <Route path="/marketplace" component={Marketplace}/>
+             {/* <Route path="/marketplace" component={() => {
+              window.location.href = 'https://0xe6b3f6b1fb201.wlbl.xyz/explore/POLYGON:0x06e6f338a2c6bc8d0578e045c3f346c112cb0a25';
+              return null;
+             }}/> */}
              <Route path="/macc" component={Mutants}/>
              <Route path="/thelab" component={Teaser}/>
            </Switch>
