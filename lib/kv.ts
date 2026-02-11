0346b5ae3b80a61bf0a68cc22d671fb0e45a0c74
cartoonitunes <181663414+cartoonitunes@users.noreply.github.com>
2026-02-10 23:00:19 -0500

    use KV_REST_API_URL/TOKEN env vars for upstash redis

--- lib/kv.ts
+++ lib/kv.ts
@@ -1,3 +1,6 @@
 import { Redis } from '@upstash/redis';
 
-export const kv = Redis.fromEnv();
+export const kv = new Redis({
+  url: process.env.KV_REST_API_URL!,
+  token: process.env.KV_REST_API_TOKEN!,
+});
